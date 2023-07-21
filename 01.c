a088dec7f4b5e3bf9933b8348dbec1ba2a1b35de
ethanbae <locing45@gmail.com>
2023-07-21 20:00:43 +0900

    [feat] 기본, 변수 문법

--- 01.c
+++ 01.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
+// 무조건 적어 줘야해용 , 안적어줘도 printf,scanf는 된다고 해용
+
+// 1 줄 주석 이에용
+
+/* 이거는 여러 줄
+주석 이에용 */
 
 int main(void)
 {
-    printf("%s, %s %s\n", "\"Hello", "C", "Language\"");
-
+    printf("%s, %s %s\n", "\"Hello", "C", "Language\"" /* 여기는 범위 주석이에용 */);
+    // \n을 띄워서 사용하면 보이지는 않지만 공백이 추가되어서 출력돼용
+    int a = 1 + 2;
     return 0;
 }
