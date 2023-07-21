cf34d041fdd0e0a8de8da7bca2113f1aaa29014c
ethanbae <locing45@gmail.com>
2023-07-21 18:56:47 +0900

    output delete

--- 01.c
+++ 01.c
@@ -2,13 +2,7 @@
 
 int main(void)
 {
-    int num01, num02;
+    printf("%s, %s %s\n", "\"Hello", "C", "Language\"");
 
-    printf("첫 번째 정수를 입력하세요 : ");
-    scanf("%d", &num01);
-    printf("두 번째 정수를 입력하세요 : ");
-    scanf("%d", &num02);
-
-    printf("합은 %d 입니다.\n", num01 + num02);
     return 0;
 }
